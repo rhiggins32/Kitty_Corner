ef8a3eac3e7cc716686cc85ba1ad07afedab80c8
root <root@DESKTOP-P0JDALQ.localdomain>
2019-03-06 13:03:57 -0600

    mock DB for JSON Server

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
-      .then(users => {this.setState({ kittens: users})});
+      .then(kittens => {this.setState({ kittens: kittens})});
   }
 
   onSearchChange = (event) => {
